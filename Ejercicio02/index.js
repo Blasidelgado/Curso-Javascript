0d820ac316e2690cc0a5b47df81c36e7fd5fabcd
Blasidelgado <blasidelgado@gmail.com>
2022-08-08 19:16:13 -0300

    Add solution exercise 02

---
+++ Ejercicio02/index.js
@@ -0,0 +1,7 @@
+const nombre = "Blas Ignacio Delgado";
+const edad = 30;
+const soyDesarrollador = true;
+const fechaNacimiento = new Date(1992, 5, 18);
+const libroFavorito = {titulo: "100 años de soledad", autor: "Gabriel García Marquez", fecha: 1967, url: "https://www.amazon.com/-/es/Gabriel-Garc%C3%ADa-M%C3%A1rquez/dp/0307474720"};
+
+// console.log (nombre, edad, soyDesarrollador, fechaNacimiento, libroFavorito);
