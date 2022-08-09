ca1e4e268268a12657077c6f2ce629dda6d4f374
Blasidelgado <blasidelgado@gmail.com>
2022-08-08 21:01:08 -0300

    Add solution exercise 03

---
+++ Ejercicio03/factorial-for.js
@@ -0,0 +1,6 @@
+let fact = 1;
+for (let i=10; i>1; i--) {
+    fact*=i;
+}
+
+console.log(fact);
---
+++ Ejercicio03/factorial-while.js
@@ -0,0 +1,8 @@
+let fact = 1;
+let i = 10;
+while (i>1) {
+    fact*=i;
+    i--;
+}
+
+// console.log(fact);
