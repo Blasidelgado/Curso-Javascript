4bd5aca06225c45996f387c3521ac26a02344d01
Blasidelgado <blasidelgado@gmail.com>
2022-08-09 20:14:42 -0300

    Add solution exercise 04

---
+++ Ejercicio04/index.js
@@ -0,0 +1,19 @@
+const nombre = "Blas Ignacio";
+const apellido = "Delgado";
+
+const estudiante = nombre.concat(' ',apellido);
+//console.log(estudiante);
+const estudianteMayus = estudiante.toUpperCase();
+//console.log(estudianteMayus);
+const estudianteMinus = estudiante.toLowerCase();
+//console.log(estudianteMinus);
+const chars = estudiante.length
+//console.log(chars);
+const firstChar = nombre[0];
+//console.log(firstChar);
+const lastChar = apellido.charAt(apellido.length - 1);
+//console.log(lastChar);
+const estudianteMod = estudiante.replace(/\s/g,'');
+//console.log(estudianteMod);
+const bool = estudiante.includes(nombre);
+//console.log(bool);
