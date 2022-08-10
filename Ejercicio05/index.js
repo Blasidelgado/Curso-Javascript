8c9cfc49f2c2c7a354517e91273d7c25b526ca80
Blasidelgado <blasidelgado@gmail.com>
2022-08-09 21:22:49 -0300

    Add solution exercise 05

---
+++ Ejercicio05/index.js
@@ -0,0 +1,10 @@
+const alturacm = 169;
+const alturamt = 1.69;
+const pesokg = 65.2;
+
+const roundAltura = Number(alturamt.toPrecision(2));
+//console.log(roundAltura);
+const roundPeso = Number(parseInt(pesokg));
+//console.log(roundPeso);
+const areEqual = (Number.MAX_VALUE) + 1 === Number.MAX_VALUE;
+//console.log(areEqual);
