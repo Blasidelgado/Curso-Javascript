414fd382f517e1757342f922fcd00a33be440b7e
Blasidelgado <blasidelgado@gmail.com>
2022-08-17 08:46:57 -0300

    Add solution exercise 07

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+const familia = new Set(["Blas", "Martina", "María", "Julián", "Soki", "Malaida", "Rusina", "Cielo"]);
+//console.log(familia);
+
+familia.add("Blas");
+//console.log(familia);
+
+familia.add("Javascript");
+//console.log(familia);
