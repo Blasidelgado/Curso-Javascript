1877fa9ffd2dcb9b92d58f0be17b0b74c7d34854
Blasidelgado <blasidelgado@gmail.com>
2022-08-19 12:08:16 -0300

    Add solution exercise 10

---
+++ Ejercicio10/index.js
@@ -0,0 +1,6 @@
+import * as controller from './controller.js';
+import chalk from 'chalk';
+
+const res = controller.multiplica(controller.suma(1,2),controller.suma(4,5));
+
+console.log(chalk.green('Hello world!'));
